415f5cb9a60bcf1ec3aad824db298768462e758c
Tom Roth <t.roth11@icloud.com>
2026-02-20 13:12:26 +0100

    location input and redirecting works

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("index.html", search_url=url_for("search_location"))
+
+@app.route("/search_location", methods=["POST"])
+def search_location():
+    plz = request.form["plz"]
+    return redirect(url_for("weather", plz=plz))
+
+@app.route("/weather/<plz>")
+def weather(plz:int):
+    return render_template("weather.html", plz=plz)
+
+def get_weather_data(plz):
+    pass
